f3acaea31f973ca3c65201e2efb4aee3b3d879eb
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-03 14:10:26 -0500

    Install typescript and add to app.js

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -3,8 +3,19 @@ import React, { useState, useEffect } from 'react'
 import getData from "../../apicalls"
 import { Route, Routes } from 'react-router-dom'
 
-const App = () => {
-  const [data, setData] = useState([])
+interface Guesses {
+  Americas: number,
+  Asia: number,
+  Oceania: number,
+  Europe: number,
+  Africa: number
+}
+
+const App: React.FC = () => {
+  const [data, setData] = useState<string[]>([])
+  const [correctGuesses, setCorrectGuesses] = useState<Guesses>({ Americas: 0, Asia: 8, Oceania: 0, Europe: 0, Africa: 0 })
+  const [incorrectGuesses, setIncorrectGuesses] = useState<Guesses>({ Americas: 0, Asia: 4, Oceania: 0, Europe: 0, Africa: 0 })
+  
   const initApp = async () => {
     try {
       const response = await getData()
@@ -14,10 +25,18 @@ const App = () => {
       console.log(error)
     }
   }
+
   useEffect(() => {
     initApp()
     console.log("data", data)
   }, [])
+
+  const keepScore = (continent: string) => {
+    const total: number = correctGuesses[continent] + incorrectGuesses[continent]
+    const score = (correctGuesses[continent]/total * 100).toFixed() + '%'
+    console.log('score', score)
+  }
+
   return (
     <main className="app-container">
       <h1>Trivia Game</h1>
