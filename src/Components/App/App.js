ba771ded233ed35c0a324ecda01af0fc3cb4cc06
Tim T <tim.email.personal@gmail.com>
2023-01-03 08:29:31 -1000

    Add react router version 6in index.js and App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import '../App/App.css'
 import React, { useState, useEffect } from 'react'
 import getData from "../../apicalls"
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   const [data, setData] = useState([])
@@ -20,6 +21,16 @@ const App = () => {
   return (
     <main className="app-container">
       <h1>Trivia Game</h1>
+      <Routes>
+        <Route
+          exact path="/"
+          element={<h2>Start view</h2>}
+        />
+        <Route
+          exact path="/play"
+          element={<h2>Play view</h2>}
+        />
+      </Routes>
     </main>
   )
 }
