65929c1817bbaa68037bab743eaeb1858dbb4a0d
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-04 15:37:49 -0500

    Add cypress tests for each continent button

--- cypress/e2e/continents.cy.ts
+++ cypress/e2e/continents.cy.ts
@@ -33,6 +33,15 @@ describe('continents spec', () => {
   it('Should have a game title', () => {
     cy.get('[data-cy="title"]').contains('Trivia Game')
   });
+  it('Should display buttons for each of the continents', () => {
+    cy.get('[href="/play/AF"] > button').contains('Africa')
+    cy.get('[href="/play/AN"] > button').contains('Antarctica')
+    cy.get('[href="/play/AS"] > button').contains('Asia')
+    cy.get('[href="/play/EU"] > button').contains('Europe')
+    cy.get('[href="/play/NA"] > button').contains('North America')
+    cy.get('[href="/play/OC"] > button').contains('Oceania')
+    cy.get('[href="/play/SA"] > button').contains('South America')
+  })
   it('Should route user to the categories page of specific continent', () => {
     cy.get('[href="/play/AF"] > button').click()
     cy.visit("http://localhost:3000/play/AF")
