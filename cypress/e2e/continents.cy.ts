8e04859327fe37de7fe03d0ae46045bcdbb423b2
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-06 16:27:57 -0500

    WIP cypress testing for continents

--- cypress/e2e/continents.cy.ts
+++ cypress/e2e/continents.cy.ts
@@ -1,41 +1,41 @@
 describe('continents spec', () => {
   beforeEach(() => {
-    cy.intercept('https://countries.trevorblades.com/').as('continents')
-    cy.visit('http://localhost:3000/play')
-    cy.wait('@continents').then((interception) => {
-      body: {
-      fixture: 'continents.json'
-     }
-    })
-    // cy.request({
+    cy.visit("http://localhost:3000/play")
+    // cy.intercept({
     //   method: 'POST',
-    //   url: 'https://countries.trevorblades.com/',
-    //   body: {
-    //     query: `
-    //   query {
-    //       continents {
-    //           code 
-    //         	name
-    //         countries { 
-    //           code
-    //           name
-    //           emoji
-    //           capital
-    //           currency
-    //           states {
-    //             name
-    //           }
-    //           languages {
-    //             name
-    //             native
-    //           }
-    //         } 
-    //       }
-    //     }
-    //   `
-    //   }
+    //   url: '/',
+    //   headers: {'x-gql-operation-name': 'countries.trevorblades.com'}
     // })
-    // cy.visit("http://localhost:3000/play")
+    // })
+   
+    cy.request({
+      method: 'POST',
+      url: 'https://countries.trevorblades.com/',
+      body: {
+        query: `
+      query {
+          continents {
+              code 
+            	name
+            countries { 
+              code
+              name
+              emoji
+              capital
+              currency
+              states {
+                name
+              }
+              languages {
+                name
+                native
+              }
+            } 
+          }
+        }
+      `
+      }
+    })
   })
   it('Should have a game title', () => {
     cy.get('[data-cy="title"]').contains('Trivia Game')
@@ -50,13 +50,14 @@ describe('continents spec', () => {
     cy.get('div > :nth-child(7)').contains('South America')
   });
   it('Should display category buttons for category options', () => {
-    cy.get('.continent-buttons > div').click({ multiple: true})
+    // cy.get('.continent-buttons').click({ multiple: true })
+    cy.get('.continent-buttons > div > :nth-child(1)').click()
     cy.get('[name="emoji"]').contains('Flags')
-    cy.get('[name="languages"]').contains('Languages')
     cy.get('[name="capitols"]').contains('Capitols')
+    cy.get('[name="languages"]').contains('Languages')
   });
   it('Should display trivia game mode', () => {
-    cy.get('.continent-buttons > div').click({ multiple: true})
+    cy.get('.continent-buttons > div :nth-child(1)').click()
     cy.get('.category-buttons').click({ multiple: true})
   })
 })
