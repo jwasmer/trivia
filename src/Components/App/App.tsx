a728bfb8483dccfcbc01ebf66fff38a9ffff2aa1
Tim T <tim.email.personal@gmail.com>
2023-01-05 17:04:18 -1000

    Write filterSelections function and create if statement that calls function when data is present

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -42,7 +42,8 @@ const App: React.FC = () => {
     const score = (correctGuesses[continent] / total * 100).toFixed() + '%'
     return score
   }
-  const assignSelections = (newSelection: object | string)  => {
+
+  const assignSelections = (newSelection: object | string) => {
     if (newSelection === 'emoji' || newSelection === 'capitols' || newSelection === 'languages') {
       setSelectedCategoryApp(newSelection)
       console.log("CATEGORY", newSelection)
@@ -51,23 +52,36 @@ const App: React.FC = () => {
       console.log("CONTINENT TYPE", newSelection)
     }
   }
+
+  const filterSelections = (category: string) => {
+    const selectedGameData = selectedContinent.countries.reduce((acc: [], curr: {}) => {
+      acc.push({ [curr.name]: curr[category] })
+      return acc
+    }, [])
+    console.log("selected game data", selectedGameData)
+  }
+
+  if (selectedCategory && selectedContinent) {
+    filterSelections(selectedCategory)
+  }
+
   return (
     <main className="app-container">
-       <NavLink to='/' className='home-link'>
+      <NavLink to='/' className='home-link'>
         <h1 className="title" data-cy="title">Trivia Game</h1>
       </NavLink>
       <Routes>
         <Route
           path="/"
           element={<div className="homepage-content">
-            <img className="earth-gif" src={'https://media.giphy.com/media/VI2UC13hwWin1MIfmi/giphy.gif'} alt="rotating earth gif" data-cy="earth-gif"/>
+            <img className="earth-gif" src={'https://media.giphy.com/media/VI2UC13hwWin1MIfmi/giphy.gif'} alt="rotating earth gif" data-cy="earth-gif" />
             <div className="home-buttons">
-            <NavLink to='/play' className='select-link'>
-              <button className="select-game" data-cy="select-game-btn">Select Game</button>
-            </NavLink>
-            <NavLink to='/scoreboard' className='scoreboard-link'>
-              <button className="view-scoreboard" data-cy="view-scoreboard-btn">View Scoreboard</button>
-            </NavLink>
+              <NavLink to='/play' className='select-link'>
+                <button className="select-game" data-cy="select-game-btn">Select Game</button>
+              </NavLink>
+              <NavLink to='/scoreboard' className='scoreboard-link'>
+                <button className="view-scoreboard" data-cy="view-scoreboard-btn">View Scoreboard</button>
+              </NavLink>
             </div>
           </div>}
         />
