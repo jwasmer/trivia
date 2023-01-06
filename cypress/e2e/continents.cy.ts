eed47b7b16cb4a5361dd904aaaef8fac50e3c574
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-06 10:14:28 -0500

    Add cypress test for category button click

--- cypress/e2e/continents.cy.ts
+++ cypress/e2e/continents.cy.ts
@@ -33,7 +33,7 @@ describe('continents spec', () => {
   it('Should have a game title', () => {
     cy.get('[data-cy="title"]').contains('Trivia Game')
   });
-  it('Should display buttons for each of the continents', () => {
+  it('Should display continent buttons for each of the continents', () => {
     cy.get('div > :nth-child(1)').contains('Africa')
     cy.get('div > :nth-child(2)').contains('Antarctica')
     cy.get('div > :nth-child(3)').contains('Asia')
@@ -41,11 +41,25 @@ describe('continents spec', () => {
     cy.get('div > :nth-child(5)').contains('North America')
     cy.get('div > :nth-child(6)').contains('Oceania')
     cy.get('div > :nth-child(7)').contains('South America')
-  })
-  it('Should display buttons for category options', () => {
+  });
+  it('Should display category buttons for category options', () => {
     cy.get('.continent-buttons > div').click({ multiple: true})
     cy.get('[name="emoji"]').contains('Flags')
     cy.get('[name="languages"]').contains('Languages')
     cy.get('[name="capitols"]').contains('Capitols')
+  });
+  it('Should display trivia game mode', () => {
+    cy.get('.continent-buttons > div').click({ multiple: true})
+    cy.get('.category-buttons').click({ multiple: true})
   })
 })
+
+
+//it('Should be able to fetch data, () => {
+//   cy.intercept('POST', 'https://countries.trevorblades.com/', (req) => {
+  //req.reply({
+// statusCode: 200,
+// fixture: 'continents.json'
+  // })
+// } )
+// })
