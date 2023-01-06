7e8d08909f58707769a21c6ead1761fb1af3e94b
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-06 11:33:43 -0500

    Add image to page

--- src/Components/Continents/Continents.tsx
+++ src/Components/Continents/Continents.tsx
@@ -16,10 +16,6 @@ interface selections {
   assignSelections: (newSelection: object | string) => void
 }
 
-type EventTarget = {
-  name: string | null
-}
-
 const Continents: React.FC<CountriesProps> = (props): JSX.Element => {
   const [selectedContinent, setSelectedContinent] = useState({})
   const contienentKeys = Object.keys(selectedContinent)
@@ -39,15 +35,18 @@ const Continents: React.FC<CountriesProps> = (props): JSX.Element => {
     setSelectedCategory(event.currentTarget.name)
   }
   return (
-    <div className='continent-buttons'>
-      {!contienentKeys.length && <div>{continentsButtons}</div>}
-      {contienentKeys.length > 0 && selectedCategory === '' ?
-        <div className='category-buttons'>
-          <button key="emoji" name="emoji" onClick={(event) => assignCategory(event)}>Flags</button>
-          <button key="capitols" name="capitols" onClick={(event) => assignCategory(event)}>Capitols</button>
-          <button key="languages" name="languages" onClick={(event) => assignCategory(event)}>Languages</button>
-        </div>
-        : null}
+    <div>
+       <img className="earth-gif" src='https://media.giphy.com/media/VI2UC13hwWin1MIfmi/giphy.gif' alt="rotating earth gif" data-cy="earth-gif"/>
+      <div className='continent-buttons'>
+        {!contienentKeys.length && <div>{continentsButtons}</div>}
+        {contienentKeys.length > 0 && selectedCategory === '' ?
+          <div className='category-buttons'>
+            <button key="emoji" name="emoji" onClick={(event) => assignCategory(event)}>Flags</button>
+            <button key="capitols" name="capitols" onClick={(event) => assignCategory(event)}>Capitols</button>
+            <button key="languages" name="languages" onClick={(event) => assignCategory(event)}>Languages</button>
+          </div>
+          : null}
+      </div>
     </div>
   )
 }
