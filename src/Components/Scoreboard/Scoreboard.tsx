0783a4a550c85b898d5a2db227b10a6bf5eb1b4a
James Wasmer <jameswasmer@gmail.com>
2023-01-06 19:50:28 -0500

    Clean up Scoreboard.tsx

--- src/Components/Scoreboard/Scoreboard.tsx
+++ src/Components/Scoreboard/Scoreboard.tsx
@@ -1,8 +1,3 @@
-// This page should capture the score held in App.tsx state and display it. The page should include the following:
-//     Average correct % overall
-//     Average correct % by continent
-
-import React, { useState } from 'react'
 import { KeepScore, Guesses, Score } from '../App/App'
 
 export default function Scoreboard({ keepScore, guesses }: {keepScore: KeepScore, guesses: Guesses}) {
