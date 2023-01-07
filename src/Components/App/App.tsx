0c9911952eb5aba8a6ce97835f6ea02a7aa14d89
Tim T <tim.email.personal@gmail.com>
2023-01-07 10:57:37 -1000

    Add typescript to selectedGameData

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -33,7 +33,7 @@ const App: React.FC = () => {
   const [data, setData] = useState<CountriesData[]>([])
   const [selectedContinent, setSelectedContinentApp] = useState<ContinentsData | any>({})
   const [selectedCategory, setSelectedCategoryApp] = useState<String>('')
-  const [gameData, setGameData] = useState([])
+  const [gameData, setGameData] = useState({})
   const [correctGuesses, setCorrectGuesses] = useState<Guesses>({ Americas: 0, Asia: 0, Oceania: 0, Europe: 0, Africa: 0 })
   const [incorrectGuesses, setIncorrectGuesses] = useState<Guesses>({ Americas: 0, Asia: 0, Oceania: 0, Europe: 0, Africa: 0 })
 
@@ -68,39 +68,13 @@ const App: React.FC = () => {
     }
   }
 
-  // type GameData = {
-  //   gameData: {}[]
-  //   continent: string
-  //   category: string
-  //   selectedGameData?: () => void
-  //   name?: string
-  // }
-
-  // type Current = {
-  //   code: string
-  //   name: string
-  //   capital: string | null
-  //   emoji: string | null
-  //   languages: {name: string, native: string}[] | null
-  //   states: []
-  // }
-
   const filterSelections = (categoryData: string) => {
-    console.log("selected continent at filter", selectedContinent)
     let gameData = []
     for (let country of selectedContinent.countries) {
-      gameData.push({ [country.name]: country[categoryData] })
+      gameData.push({ [country.name]: country[categoryData], usedInQuestion: false })
     }
-    const selectedGameData: any = {gameData: gameData, continent: selectedContinent.name, category: categoryData}
-      // const selectedGameData = selectedContinent.countries.reduce<GameData>((acc: GameData, curr: Current | any) => {
-      //   console.log('CURRENT CURRENT CURRENT', curr)
-      //   // console.log('CURRENT CATEGORY DATA', curr[categoryData])
-      //   acc.gameData.push({ [curr.name]: curr[categoryData] })
-      //   return acc
-      // }, { gameData: [], continent: selectedContinent.name, category: categoryData })
-      // console.log("selected game data", selectedGameData)
-      setGameData(selectedGameData)
-      console.log('SELECTED GAME DATA', gameData)
+    const selectedGameData: { gameData: [] | unknown, continent: string, category: string | [] } = { gameData: gameData, continent: selectedContinent.name, category: categoryData }
+    setGameData(selectedGameData)
   }
 
   return (
