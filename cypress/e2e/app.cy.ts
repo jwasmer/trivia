2c15d7b48a23f2807fcf192152f88a069dc8a7c4
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-08 18:47:32 -0500

    Update cypress tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,14 +1,22 @@
 describe('template spec', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/")
+    cy.intercept('https://countries.trevorblades.com/',
+    cy.stub()
+      .callsFake(req => req.reply({ fixture: 'continents.json' }))).as('continents')
+      cy.visit('http://localhost:3000/') 
+      cy.wait('@continents')
   })
 
   it('Should load title text', () => {
     cy.get('[data-cy="title"]').should("exist")
+    cy.get('[data-cy="title"]').contains('Trivia Game')
+    cy.get('[data-cy="select-game-btn"]').contains('Select Game')
+    cy.get('[data-cy="view-scoreboard-btn"]').contains('View Scoreboard')
   })
 
   it('Should load main page graphic', () => {
     cy.get('[data-cy="earth-gif"]').should("exist")
+    cy.get('[data-cy="earth-gif"]').should('have.attr', 'src').should('include','https://media.giphy.com/media/VI2UC13hwWin1MIfmi/giphy.gif')
   })
 
   it('Should route user to game options page when "select game" button is clicked', () => {
