a943b51dbac31aaa38791055b35342f3511482d9
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-01-08 17:59:08 -0500

    Update pathnames to match with routes

--- cypress/e2e/continents.cy.ts
+++ cypress/e2e/continents.cy.ts
@@ -3,7 +3,7 @@ describe('continents spec', () => {
    cy.intercept('https://countries.trevorblades.com/',
     cy.stub()
       .callsFake(req => req.reply({ fixture: 'continents.json' }))).as('continents')
-      cy.visit('http://localhost:3000/play') 
+      cy.visit('http://localhost:3000/selections') 
       cy.wait('@continents')
   })
    
@@ -13,6 +13,8 @@ describe('continents spec', () => {
     cy.get('.continent-button').contains('Antarctica')
   });
   it('Should display category buttons for category options', () => {
+    cy.get('[data-cy="title"]').contains('Trivia Game')
+    cy.get('[data-cy="earth-gif"]').should('have.attr', 'src').should('include','https://media.giphy.com/media/VI2UC13hwWin1MIfmi/giphy.gif')
     cy.get('.continent-button').click()
     cy.get('[name="emoji"]').contains('Flags')
     cy.get('[name="capitols"]').contains('Capitols')
@@ -20,9 +22,11 @@ describe('continents spec', () => {
     cy.get('[name="emoji"]').click()
   });
   it('Should display trivia game mode', () => {
+    cy.get('[data-cy="title"]').contains('Trivia Game')
+    cy.get('[data-cy="earth-gif"]').should('have.attr', 'src').should('include','https://media.giphy.com/media/VI2UC13hwWin1MIfmi/giphy.gif')
     cy.get('.continent-button').click()
     cy.get('[name="emoji"]').click()
-    // cy.location("pathname").should("eq", "/game")
+    cy.location("pathname").should("eq", "/play")
   })
 })
 
