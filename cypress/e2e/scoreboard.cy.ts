ee130281f07f4582c28d996fdd7775e82658facb
James Wasmer <jameswasmer@gmail.com>
2023-01-09 15:09:41 -0500

    Test that all score fields are rendered

--- cypress/e2e/scoreboard.cy.ts
+++ cypress/e2e/scoreboard.cy.ts
@@ -6,3 +6,15 @@ describe('continents spec', () => {
       cy.visit('http://localhost:3000/selections') 
       cy.wait('@continents')
   })
+
+  it('Should have score fields for each continent', () => {
+    cy.get('[data-cy="north-america"]').should("exist")
+    cy.get('[data-cy="south-america"]').should("exist")
+    cy.get('[data-cy="oceania"]').should("exist")
+    cy.get('[data-cy="asia"]').should("exist")
+    cy.get('[data-cy="europe"]').should("exist")
+    cy.get('[data-cy="africa"]').should("exist")
+  })
+
+  
+})
